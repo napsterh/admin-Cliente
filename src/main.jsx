8ad8f0e12e4e64c605241eff723295abcf4b0316
Napster <rome.1205h@gmail.com>
2023-10-20 18:49:37 -0500

    156 add pages and outlet

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout'
+import NuevoCliente from './pages/NuevoCliente'
+import Index from './pages/Index'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <h1>Inicio</h1>
+    element: <Layout/>,
+    children: [
+      {
+        index: true,
+        element: <Index/>
+      },
+      {
+        path: '/clientes/nuevo',
+        element: <NuevoCliente/>
+      }
+    ]
   },
-  {
-    path: '/nosotros',
-    element: <h1>Nosotros</h1>
-  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
