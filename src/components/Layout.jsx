23d91b9b3d9b38ba3176d4ec780f8561fc4a849e
Napster <rome.1205h@gmail.com>
2023-10-24 00:00:22 -0500

    fix columnas

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,15 @@
 import { Outlet } from 'react-router-dom'
+import 'tailwindcss/tailwind.css'
 
 function Layout() {
   return (
    <div className='md:flex md:min-h-screen'>
       <div className='md:w-1/4 bg-blue-900 px-5 py-10'>
-
+         <h2 className='text-4xl font-black text-center text-white'>CRM - Clientes</h2>
       </div>
 
       <main className='md:w-3/4 p-10 md:h-screen overflow-scroll'>
-
+         <Outlet/>
       </main>
    </div>
   )
